182ef0d36848508a0fde0012427e814aea46d371
ajwang-kajwang <61467419+ajwang-kajwang@users.noreply.github.com>
2025-09-10 17:19:15 +0800

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,7 @@
+use rocket::fs::{FileServer, Options};
+use rocket::launch;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", FileServer::new(rocket::fs::relative!("static"), Options::Index))
+}
